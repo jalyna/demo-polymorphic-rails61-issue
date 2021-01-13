6d3e6eeb6dc54aeecb0e568ed28d9d09680c3f85
Jalyna <jalyna.schroeder@gmail.com>
2021-01-13 13:45:12 +0100

    Add comment

--- test/models/subscription_test.rb
+++ test/models/subscription_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SubscriptionTest < ActiveSupport::TestCase
   test "loads no subscription when no plans are given (polymorphic belongs-to)" do
-    assert_nil Subscription.find_by(plan: [])
+    assert_nil Subscription.find_by(plan: []) # fails on 6.1.0 and 6.1.1
   end
 
   test "loads no subscription when no public plans are given (not polymorphic belongs-to)" do
